b08528393a8308224334ead15f85db01247e8c61
rajniparajuli <rajaniparajuli18@gmail.com>
2017-09-03 23:28:14 +1000

    return value changed

--- bcccp/tickets/season/UsageRecordFactory.java
+++ bcccp/tickets/season/UsageRecordFactory.java
@@ -5,7 +5,7 @@ public class UsageRecordFactory implements IUsageRecordFactory {
 	@Override
 	public IUsageRecord make(String ticketId, long startDateTime) {
 		// TODO Auto-generated method stub
-		return null;
+		return;
 	}
 
 
