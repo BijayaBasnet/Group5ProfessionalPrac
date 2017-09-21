fcd5ca2a1216ed9d18e1abbb54da598cc60a0088
BijayaBasnet <30451163+BijayaBasnet@users.noreply.github.com>
2017-09-21 21:22:39 +1000

    java structure code -junit test for seasonticket
    this is the skeleton of j unit test for season ticket

--- TestByBijaya/SeasonTicketTest.java
+++ TestByBijaya/SeasonTicketTest.java
@@ -1,1 +1,28 @@
+package testcases;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import bcccp.tickets.season.ISeasonTicket;
+import bcccp.tickets.season.SeasonTicket;
+
+public class SeasonTicketTest {
+  @Before
+	public void setUp() throws Exception {
+    }
+  @After
+	public void tearDown() throws Exception {
+		
+	}
+  
+	@Test
+	public void testInit() {
+    	}
+
+}
