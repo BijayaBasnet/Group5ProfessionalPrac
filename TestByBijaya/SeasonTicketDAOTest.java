09faed908fcdb7d64d1813fd4bfc21e72a512b34
BijayaBasnet <30451163+BijayaBasnet@users.noreply.github.com>
2017-09-22 22:24:10 +1000

    creating skeletion junit test for seasonticketDao

---
+++ TestByBijaya/SeasonTicketDAOTest.java
@@ -0,0 +1,59 @@
+package testcases;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+
+import java.util.Date;
+
+import bcccp.tickets.season.ISeasonTicket;
+import bcccp.tickets.season.ISeasonTicketDAO;
+import bcccp.tickets.season.IUsageRecord;
+import bcccp.tickets.season.IUsageRecordFactory;
+import bcccp.tickets.season.SeasonTicket;
+import bcccp.tickets.season.SeasonTicketDAO;
+import bcccp.tickets.season.UsageRecord;
+import bcccp.tickets.season.UsageRecordFactory;
+
+public class SeasonTicketDAOTest {
+	
+	
+
+	@Before
+	public void setUp() throws Exception {
+		
+		
+		
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		}
+
+	@Test
+	public void testRegisterSeasonTicket() {
+		
+	}
+	
+	@Test
+	public void testDeregisterSeasonTicket() {
+		s
+	}
+	
+	@Test
+	public void testFindSeasonTicketByID() {
+		
+	}
+	
+	@Test
+	public void testRecordTicketEntry() {
+		
+	}
+
+}
