f785289b4fd2f133bda04c0089a446c52be0d757
Binaya Subedi <binaya.subedi1@gmail.com>
2017-09-23 13:24:23 +1000

    Added test methods to check car capacity, issue adhoc ticket, check season ticket validity

--- test/CarparkTest.java
+++ test/CarparkTest.java
@@ -1,19 +1,25 @@
 package testcases;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
-
 import bcccp.carpark.Carpark;
 import bcccp.carpark.ICarpark;
 import bcccp.carpark.ICarparkObserver;
 import bcccp.carpark.entry.EntryController;
+import bcccp.tickets.adhoc.AdhocTicket;
 import bcccp.tickets.adhoc.AdhocTicketDAO;
+import bcccp.tickets.adhoc.IAdhocTicket;
 import bcccp.tickets.adhoc.IAdhocTicketDAO;
+import bcccp.tickets.season.ISeasonTicket;
 import bcccp.tickets.season.ISeasonTicketDAO;
+import bcccp.tickets.season.SeasonTicket;
 import bcccp.tickets.season.SeasonTicketDAO;
 
 public class CarparkTest {
@@ -21,6 +27,8 @@ public class CarparkTest {
 	ICarparkObserver carparkObserver;
 	ISeasonTicketDAO seasonTicketDAO;
 	IAdhocTicketDAO adhocTicketDAO;
+	IAdhocTicket adhocTicket;
+	ISeasonTicket seasonTicket;
 	ICarpark carpark;
 	String carparkId;
 	int numberOfCapacity;
@@ -31,10 +39,17 @@ public class CarparkTest {
 		carparkObserver = Mockito.mock(EntryController.class);
 		seasonTicketDAO = Mockito.mock(SeasonTicketDAO.class);
 		adhocTicketDAO = Mockito.mock(AdhocTicketDAO.class);
-		carparkId = "Bathurst";
-		numberOfCapacity = 3;
+		adhocTicket = Mockito.mock(AdhocTicket.class);
+		seasonTicket = Mockito.mock(SeasonTicket.class);
+		long currentDateTime = new Date().getTime() + 300000;
 		
-		ICarpark carpark = new Carpark(carparkId, numberOfCapacity, adhocTicketDAO, seasonTicketDAO);
+		Mockito.when(adhocTicketDAO.createTicket("Bathurst")).thenReturn(adhocTicket);
+		Mockito.when(seasonTicketDAO.findTicketById("1")).thenReturn(seasonTicket);
+		Mockito.when(seasonTicket.getEndValidPeriod()).thenReturn(currentDateTime);
+		carparkId = "Bathurst";
+		numberOfCapacity = 1;
+		
+		carpark = new Carpark(carparkId, numberOfCapacity, adhocTicketDAO, seasonTicketDAO);
 		
 		
 	}
@@ -59,5 +74,23 @@ public class CarparkTest {
 	public void testConstructorWithSeasonTicketDAONull() {
 		carpark = new Carpark(carparkId, numberOfCapacity, adhocTicketDAO, null);
 	}
+	
+	@Test
+	public void testisFull() {
+		carpark.register(carparkObserver);
+		assertEquals(carpark.isFull(), true);
+	}
+	
+	@Test
+	public void testIssueAdhocTicket() {
+		assertTrue(carpark.issueAdhocTicket() instanceof IAdhocTicket);
+		
+	}
+	
+	@Test
+	public void testIsSeasonTicketValid() {
+		assertTrue(carpark.isSeasonTicketValid("1"));
+	}
 
-}
+		
+	}
