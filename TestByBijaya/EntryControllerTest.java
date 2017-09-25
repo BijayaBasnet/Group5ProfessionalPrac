d9ded6f4fa36ad8eaaa2e37cdb2a0179f6a12f74
BijayaBasnet <30451163+BijayaBasnet@users.noreply.github.com>
2017-09-25 21:33:45 +1000

    creating junit skeleton for EntryControllerTest

---
+++ TestByBijaya/EntryControllerTest.java
@@ -0,0 +1,47 @@
+package testcases;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import bcccp.carpark.CarSensor;
+import bcccp.carpark.Carpark;
+import bcccp.carpark.Gate;
+import bcccp.carpark.ICarSensor;
+import bcccp.carpark.ICarpark;
+import bcccp.carpark.IGate;
+import bcccp.carpark.entry.EntryController;
+import bcccp.carpark.entry.EntryUI;
+import bcccp.carpark.entry.IEntryController;
+import bcccp.carpark.entry.IEntryUI;
+import bcccp.tickets.adhoc.AdhocTicket;
+import bcccp.tickets.adhoc.IAdhocTicket;
+
+public class EntryControllerTest {
+	
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+
+	@Test
+	public void testCarDetected() {
+		
+	}
+	
+	public void testGateRaise() {
+		
+	}
+	
+
+}
