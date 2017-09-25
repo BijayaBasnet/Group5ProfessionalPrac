103f2ac8d0744fe0ca2f19809883d724029e6d48
rajniparajuli <rajaniparajuli18@gmail.com>
2017-09-25 21:35:50 +1000

    paystation testing

---
+++ test_rajani/PayStationControllerTest.java
@@ -0,0 +1,29 @@
+package testcases;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import static org.mockito.ArgumentMatchers.anyLong;
+
+import bcccp.carpark.Carpark;
+import bcccp.carpark.ICarpark;
+import bcccp.carpark.entry.EntryUI;
+import bcccp.carpark.paystation.IPaystationUI;
+import bcccp.carpark.paystation.PaystationUI;
+import bcccp.tickets.adhoc.AdhocTicket;
+import bcccp.tickets.adhoc.IAdhocTicket;
+
+public class PayStationControllerTest {
+	
+	
+	IPaystationUI ui;	
+	ICarpark carpark;
+
+	
+
+}
