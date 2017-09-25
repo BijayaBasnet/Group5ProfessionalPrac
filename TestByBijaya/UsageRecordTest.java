4955e018fcce69bb38ec0724a577683921eb63a1
BijayaBasnet <30451163+BijayaBasnet@users.noreply.github.com>
2017-09-25 21:24:19 +1000

    Junit skeletion for UsageRecord Test

---
+++ TestByBijaya/UsageRecordTest.java
@@ -0,0 +1,27 @@
+package testcases;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import bcccp.tickets.season.UsageRecord;
+
+public class UsageRecordTest {
+	
+	@Before
+	public void setUp() throws Exception {	
+	}
+
+	@After
+	public void tearDown() throws Exception {
+			}
+
+	@Test
+	public void test() {
+	}
+
+}
